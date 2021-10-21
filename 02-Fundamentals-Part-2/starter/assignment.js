b2d3f4efdc44fb3ef3a8929d7a53fc81da1f69d8
Jihad Assami <jassami1@hotmail.com>
2021-10-21 16:59:14 -0500

    fundamentas2 part 1

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,66 @@
+'use strict';
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
+// }
+// const country1 = describeCountry('Syria', 23, 'Damascus');
+// const country2 = describeCountry('Jordan', 14, 'Amman');
+// const country3 = describeCountry('France', 60, 'Paris');
+// console.log(country1);
+// console.log(country2);
+// console.log(country3);
+
+// function percentageOfWorld1(population) {
+//     return population / 7900 * 100;
+// }
+// const case1 = percentageOfWorld1(1441);
+// console.log(case1);
+
+// const percentageOfWorld2 = function (population) {
+//     return population / 7900 * 100;
+// }
+// const case2 = percentageOfWorld2(23);
+// console.log(case2);
+// console.log(percentageOfWorld2(23));
+
+// const percentageOfWorld3 = population => population / 7900 * 100;
+// console.log(percentageOfWorld3(1441));
+
+
+// function percentageOfWorld1(population) {
+//     return population / 7900 * 100;
+// }
+
+// function describePopulation(country, population) {
+//     const caseCountry = percentageOfWorld1(population);
+//     return `${country} has ${population} million people, which is about ${caseCountry} of the world.`
+// }
+// console.log(describePopulation('China', 1441));
+// console.log(describePopulation('France', 60));
+
+
+
+// const populations = [1441, 23, 60, 34];
+// if (populations.length === 4) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// };
+// const calcPer = population => population / 7900 * 100;
+// const percentages = [calcPer(populations[0]), calcPer(populations[1]), calcPer(populations[2]), calcPer(populations[3])];
+// console.log(percentages);
+
+
+// const neighbours = ['Jordan', 'Iraq', 'Lebanon', 'Turky'];
+// neighbours.push('Utopia');
+// console.log(neighbours);
+// neighbours.pop();
+// console.log(neighbours);
+// if (!neighbours.includes('Germany')) {
+//     console.log('Probably not a central European country');
+// }
+// neighbours[1] = 'Republic of Iraq';
+// console.log(neighbours);
+// neighbours[neighbours.indexOf('Jordan')] = 'Jordanian Kingdom';
+// console.log(neighbours);
+
+
