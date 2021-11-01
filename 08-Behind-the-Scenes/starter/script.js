2d61b02742b22648786b6bce4d4684d23f849adb
Jihad Assami <jassami1@hotmail.com>
2021-10-31 21:23:13 -0500

    JS behind the scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
+
+//     function printAge() {
+//         let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//         console.log(output);
+
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true;
+//             const str = `Oh, and you're a millenial, ${firstName}`;
+//             console.log(str);
+
+//             function add(a, b) {
+//                 return a + b;
+//             }
+//             const output = 'New output';
+//         }
+//         console.log(millenial);
+//         console.log(output);
+//         // console.log(add(2, 3)); this will only work if the strict mode is of, otherwise the function is not defined here.
+//     }
+//     printAge();
+
+//     return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+//  Hoisting and TDZ
+//  hosting with variables
+// console.log(me);
+// // console.log(job);
+// console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+//  Hoisting with functions: declarational, expertion, and arrow functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//     return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+// example for fitfull of hoisting
+
+// if (!numProducts) deleteShoppingCart();
+// var numProducts = 10;
+// function deleteShoppingCart() {
+//     console.log('All products are deleted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+
