ac0e85622c351bdf49dc12df6682b5f5e0780eeb
Jihad Assami <jassami1@hotmail.com>
2021-11-01 22:06:09 -0500

    this keyword $ arguments keywor

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -69,3 +69,86 @@
 // console.log(x === window.x);
 // console.log(y === window.y);
 
+// The this keyword
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+// calcAge(1991);
+
+// const calcArrow = (birthYear) => {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+// calcArrow(1985);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     }
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// }
+
+// matilda.calcAge = jonas.calcAge;
+// console.log(matilda);
+// // this is method borrowing
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// console.log(f);
+// f();
+
+
+// var firstName = 'Matilda';
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     },
+//     greet: () => {
+//         console.log(this);
+//         console.log(`Hey ${this.firstName}`);
+//     },
+// };
+// jonas.greet();
+
+
+const jonas = {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function () {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        // const self = this;
+        // const isMellenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+
+        const isMellenial = function () {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+
+        isMellenial();
+    },
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
+    },
+};
+jonas.greet();
+jonas.calcAge();
+
