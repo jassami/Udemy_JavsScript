d7e9e47b4edff13ec9abc07b3e83607848d3f797
Jihad Assami <jassami1@hotmail.com>
2021-11-29 19:00:54 -0600

    oop challenge#1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,146 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    // create a property on this keyword
+    // Instance poroperties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+//  1. new {} is created
+//  2. function is called, this= {}
+//  3. {} linked to prototype
+//  4. function automatically return {} but filled 
+const may = new Person('May', 2013);
+const jack = new Person('Jack', 1987);
+console.log(may, jack);
+console.log(jonas instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__.__proto__);
+console.log(Person.prototype.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 9, 4, 5, 6, 9, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+}
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+// Coding chalenge #1
+// Data car 1: 'BMW' going at 120 km/h
+// Data car 2: 'Mercedes' going at 95 km/h
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`This ${this.make} is going ${this.speed} k/m per hour.`);
+};
+
+Car.prototype.break = function () {
+    console.log(this.speed - 5);
+};
+const bmw = new Car('BMW', 120);
+const mer = new Car('Mercedes', 95);
+bmw.accelerate();
+bmw.break();
+mer.accelerate();
+mer.break();
+
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    }
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+    set fullName(name) {
+        console.log(name);
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`)
+    }
+    get fullName() {
+        return this._fullName;
+    }
+
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+// PersonCl.prototype.greet = function () {
+//     console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+    owner: 'Jonas',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov)
+    }
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+
+
+
+
+
+
+
+
+
+
